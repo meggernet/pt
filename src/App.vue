ed5971bef9f716199e74dd0fe4a17aa4b6f4c37c
Tobias Lueckel <lueckel@tobias-lueckel.de>
2023-09-24 00:33:53 +0200

    Added darkMode support

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="m-4 p-4 mx-auto">
+  <div class="bg-white dark:bg-black p-2 mx-auto">
     <router-view />
+    <hr className="m-2" />
+    <ThemeSwitcher />
   </div>
 </template>
+
+<script lang="ts">
+import ThemeSwitcher from "@/components/ThemeSwitcher.vue";
+import { Options, Vue } from "vue-class-component";
+import { checkTheme } from "@/services/ThemeSwitcherService";
+
+checkTheme();
+
+@Options({
+  components: {
+    ThemeSwitcher,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 @tailwind base;
--- src/components/Hours.vue
+++ src/components/Hours.vue
@@ -1,21 +1,33 @@
 <template>
-  <div className="items-center text-center">
-    <h1 className="w-full text-gray-900 font-semibold mb-4">Hours</h1>
+  <div className="bg-gray-100 dark:bg-gray-900 items-center text-center p-2">
+    <h1 className="w-full text-gray-900 dark:text-gray-200 font-semibold mb-4">
+      Hours
+    </h1>
     <form>
       <div className="w-full mx-auto flex flex-col mb-2">
-        <label htmlFor="hours">Input</label>
+        <label
+          htmlFor="hours"
+          className="mb-1 text-gray-900 dark:text-gray-200"
+        >
+          Input
+        </label>
         <input
           v-model="inputString"
           type="text"
           name="hours"
           id="hours"
           placeholder="1w 3d 4h"
-          className="p-2"
+          className="p-2 bg-gray-50 dark:bg-gray-950 text-gray-900 dark:text-gray-200 rounded"
         />
       </div>
       <div className="flex mb-2">
         <div className="w-1/2 mx-auto flex flex-col items-center">
-          <label htmlFor="onePThasHours-6"> 1PT=6H </label>
+          <label
+            htmlFor="onePThasHours-6"
+            className="text-gray-900 dark:text-gray-200"
+          >
+            1PT=6H
+          </label>
           <input
             v-model="onePThasHours"
             type="radio"
@@ -25,7 +37,12 @@
           />
         </div>
         <div className="w-1/2 mx-auto flex flex-col items-center">
-          <label htmlFor="onePThasHours-8"> 1PT=8H </label>
+          <label
+            htmlFor="onePThasHours-8"
+            className="text-gray-900 dark:text-gray-200"
+          >
+            1PT=8H
+          </label>
           <input
             v-model="onePThasHours"
             type="radio"
@@ -40,30 +57,36 @@
     <div className="w-full mx-auto mb-2 flex">
       <div className="w-1/6"></div>
       <div className="w-2/6">
-        <span className="text-red-500">TotalHours</span>
+        <span className="text-green-500">TotalHours</span>
       </div>
       <div className="w-2/6">
-        <span>{{ calculatedHours }}H</span>
-      </div>
-      <div className="w-1/6"></div>
-    </div>
-    <div className="w-full mx-auto mb-2 flex">
-      <div className="w-1/6"></div>
-      <div className="w-2/6">
-        <span className="text-red-500">1PT=6H</span>
-      </div>
-      <div className="w-2/6">
-        <span className="outputPThasHours-6">{{ this.calculatedPTs6 }}</span>
+        <span className="text-gray-900 dark:text-gray-200">
+          {{ calculatedHours }}H
+        </span>
       </div>
       <div className="w-1/6"></div>
     </div>
     <div className="w-full mx-auto mb-2 flex">
       <div className="w-1/6"></div>
       <div className="w-2/6">
-        <span className="text-red-500">1PT=8H</span>
+        <span className="text-green-500">1PT=6H</span>
       </div>
       <div className="w-2/6">
-        <span className="outputPThasHours-8">{{ this.calculatedPTs8 }}</span>
+        <span className="text-gray-900 dark:text-gray-200 outputPThasHours-6">
+          {{ this.calculatedPTs6 }}
+        </span>
+      </div>
+      <div className="w-1/6"></div>
+    </div>
+    <div className="w-full mx-auto mb-2 flex">
+      <div className="w-1/6"></div>
+      <div className="w-2/6">
+        <span className="text-green-500">1PT=8H</span>
+      </div>
+      <div className="w-2/6">
+        <span className="text-gray-900 dark:text-gray-200 outputPThasHours-8">
+          {{ this.calculatedPTs8 }}
+        </span>
       </div>
       <div className="w-1/6"></div>
     </div>
