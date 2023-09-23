00c507a78ea6582c0d44b655fe0ce8685aaf8078
Tobias Lueckel <lueckel@tobias-lueckel.de>
2023-09-23 23:45:13 +0200

    change e2e test to match current application

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe("My First Test", () => {
+describe("Hours test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+    cy.contains("h3", "Hours");
   });
 });
