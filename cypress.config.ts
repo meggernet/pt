3838ec2b7de2f5f0d2a4dfc42541d7db449f9be0
Tobias Lueckel <lueckel@tobias-lueckel.de>
2023-09-26 23:22:40 +0200

    migrate to new cypress version

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  fixturesFolder: "tests/e2e/fixtures",
+  screenshotsFolder: "tests/e2e/screenshots",
+  videosFolder: "tests/e2e/videos",
+  e2e: {
+    specPattern: "tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}",
+    fixturesFolder: "tests/e2e/fixtures",
+    screenshotsFolder: "tests/e2e/screenshots",
+    videosFolder: "tests/e2e/videos",
+    supportFile: "tests/e2e/support/index.js",
+  },
+});
